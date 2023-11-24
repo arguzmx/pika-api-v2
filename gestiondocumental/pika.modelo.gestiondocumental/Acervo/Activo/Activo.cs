996c1ab9e7e5c0a7e3ec3d2c3c564eb79f4aea47
Oswaldo Diaz <oswaldo.dgmx@gmail.com>
2023-11-24 11:43:05 -0600

    ModeloCuadroClasificacion

--- gestiondocumental/pika.modelo.gestiondocumental/Acervo/Activo/Activo.cs
+++ gestiondocumental/pika.modelo.gestiondocumental/Acervo/Activo/Activo.cs
@@ -268,5 +268,11 @@ public class Activo
     [JsonIgnore]
     [XmlIgnore]
     public Archivo ArchivoOrigen { get; set; }
+
+
+    [JsonIgnore]
+    [XmlIgnore]
+    public CuadroClasificacion CuadroClasificacion { get; set; }
+
 }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
---
+++ gestiondocumental/pika.modelo.gestiondocumental/CuadroClasificacion/CuadroClasificacion.cs
@@ -0,0 +1,52 @@
+﻿using api.comunes.metadatos.atributos;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace pika.modelo.gestiondocumental;
+
+[Entidad()]
+public class CuadroClasificacion 
+{
+    /// <summary>
+    /// Identificador únioc del cuadro de clasificación
+    /// </summary>
+    [Id]
+    [Formulario(indice: 1, visible: false)]
+    [Tabla(indice: 0, visible: false)]
+    public string Id { get ; set ; }
+    // [a] [d] 
+    // R 128
+
+
+    /// <summary>
+    /// Nombre único del cuadro de clasificación
+    /// </summary>
+    [Nombre]
+    [Formulario(indice: 1, ancho: 100)]
+    [Tabla(indice: 1)]
+    public string Nombre { get ; set ; }
+
+    /// <summary>
+    /// Dominio al que pertenece el archivo
+    /// </summary>
+    [Protegido]
+    public string DominioId { get; set; }
+    // Este valor simpre viene del contexto
+    // R 128
+
+    /// <summary>
+    /// Unidad organizacional a la que pertenece el archivo
+    /// </summary>
+    [Protegido]
+    public string UOrgId { get; set; }
+    //  Este valor simpre viene del contexto
+    // R 128
+
+
+    /// <summary>
+    /// Activos que utilizan el cuadro
+    /// </summary>
+    [JsonIgnore, XmlIgnore]
+    public List<Activo> Activos { get; set; }
+
+}
